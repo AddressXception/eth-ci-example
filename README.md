e919ba897ac5c8c209c355cdc6049ddb0a199ea3
Matt Wilhelm <github@addressxception.com>
2019-06-03 11:17:18 -0400

    Add ganache network, update eth-ci project
    update readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # eth-ci-example
 
 This is an example of working with [eth-ci](https://github.com/AddressXception/eth-ci)
+
+It is just the [Truffle Metacoin Box](https://github.com/truffle-box/metacoin-box) with [eth-ci](https://github.com/AddressXception/eth-ci) added as a submodule 
+
+## To Test
+
+1. Clone the repo and initialize submodules
+2. run `./ci/build.sh && ./ci/run.sh`
--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  // Uncommenting the defaults below 
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  /*
+  // Uncommenting the defaults below provides for an easier quick-start with
+  // Ganache. You can also follow this format for other networks; see
+  // <http://truffleframework.com/docs/advanced/configuration> for more details on
+  // how to specify configuration options!
+
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    ganache: {
+      host: 'ganache',
+      port: 8545,
+      network_id: '*' // Match any network id
     },
     test: {
       host: "127.0.0.1",
@@ -17,5 +21,4 @@ module.exports = {
       network_id: "*"
     }
   }
-  */
 };
